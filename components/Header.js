024036d68b92a4e00b23f2dd01ffdfa6646fdc16
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-04-12 11:58:15 +0430

    Add Header Component

---
+++ components/Header.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// Locals
+import { NarrowContainer, Wrapper } from './shared/Containers'
+import { Nav } from './Nav'
+import { Space } from './shared/Space'
+import { LaptopSet } from './vectors/LaptopSet'
+import { SectionHeading } from './shared/SectionHeading'
+import { SectionSubtitle } from './shared/SectionSubtitle'
+import { RequestAccess } from './shared/RequestAccess'
+import { TopShare } from './TopShare'
+import { UnderSign } from './shared/UnderSign'
+
+export const Header = () => (
+  <NarrowContainer>
+    <Space height={46} />
+    <Nav />
+
+    <Space height={63} />
+    <StyledWrapper>
+      <LaptopSet />
+
+      <Space height={50} />
+      <SectionHeading>
+        The base for collaborating clearly & smoothly in remote teams
+      </SectionHeading>
+
+      <Space height={23} />
+      <SectionSubtitle>
+        Schedule a meeting. Who’s asleep? Who’s on holidays? What’s the team
+        doing right now? When will this coworker be back? And more. Sign up for
+        early access.
+      </SectionSubtitle>
+
+      <Space height={33} />
+      <RequestAccess />
+
+      <Space height={52} />
+      <TopShare />
+
+      <Space height={184} />
+      <SectionHeading maxWidth={410}>
+        Rebuilt from scratch. For <UnderSign> remote-first teams.</UnderSign>
+      </SectionHeading>
+
+      <Space height={23} />
+      <SectionSubtitle>
+        The age of remote-first companies has unique requirements. There is the
+        missing half of our communication tools. A new paradigm for helping with
+        scheduling and time differences. Providing the foundation to work
+        together remotely, smoothly and closely.
+      </SectionSubtitle>
+      <Space height={134} />
+    </StyledWrapper>
+  </NarrowContainer>
+)
+
+const StyledWrapper = styled(Wrapper)`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
