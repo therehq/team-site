a4f24fa25d0c4a9a3b6ec2aa5f29a20805ec26b4
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-04-13 08:40:36 +0430

    Add RequestAccess component

---
+++ components/shared/RequestAccess.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const RequestAccess = () => (
+  <Wrapper>
+    <InputBox type="text" name="email" placeholder="Your email" />
+    <RequestButton>Request Access</RequestButton>
+  </Wrapper>
+)
+
+const Wrapper = styled.div`
+  width: 355px;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  background: #ffffff;
+  border: 1px solid #699ad4;
+  box-sizing: border-box;
+  border-radius: 3px;
+`
+
+const RequestButton = styled.button`
+  padding: 0px 10px 0 10px;
+  height: 40px;
+  border: none;
+  outline: none;
+
+  font-family: ${p => p.theme.fontText};
+  font-style: normal;
+  text-align: center;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 1.125;
+  color: #ffffff;
+
+  background: #1c84ff;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.09);
+  border-radius: 0px 3px 3px 0px;
+  cursor: pointer;
+  transition: 200ms;
+
+  :hover {
+    padding: 0px 15px 0 15px;
+    background: #aa84ff;
+  }
+`
+
+const InputBox = styled.input`
+  flex-grow: 1;
+  margin-left: 15px;
+  display: block;
+
+  border: none;
+  outline: none;
+  font-family: ${p => p.theme.fontText};
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 1.38;
+
+  color: #9fa2aa;
+`
