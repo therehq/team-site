e37d37626fbc55d5fafc4f3646ddfd0199b7b697
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-04-13 08:37:52 +0430

    Add Logo component

---
+++ components/vectors/logos/Zeit.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Zeit = props => (
+  <svg width={163} height={38} fill="none" {...props}>
+    <g
+      clipPath="url(#Zeit__clip0)"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      fill="#000"
+    >
+      <path d="M19.29 1l19.29 35.429H0L19.29 1zM61.157 28.14h14.585v-2.62H64.794l10.77-15.911V7.32H61.334v2.621h10.591l-10.77 15.91v2.29zm29.847 0h13.087v-2.62H93.964v-6.682h8.808v-2.621h-8.808V9.94h10.127V7.32H91.004v20.82zm28.849 0h12.766v-2.62h-4.885V9.94h4.885V7.32h-12.766v2.621h4.921v15.578h-4.921v2.621zm33.77 0h2.96V9.94h6.846v-2.62h-16.582v2.621h6.776v18.2z" />
+    </g>
+    <defs>
+      <clipPath id="Zeit__clip0">
+        <path fill="#fff" d="M0 0h163v38H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+)
+
+export default Zeit
---
+++ components/vectors/share/Copy.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Copy = props => (
+  <svg width="1em" height="1em" viewBox="0 0 22 21" fill="none" {...props}>
+    <path
+      d="M18.333 7.875h-8.25c-1.012 0-1.833.784-1.833 1.75V17.5c0 .966.82 1.75 1.833 1.75h8.25c1.013 0 1.834-.784 1.834-1.75V9.625c0-.966-.821-1.75-1.834-1.75z"
+      stroke="#8A99B1"
+      strokeWidth={3}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M4.583 13.125h-.916c-.487 0-.953-.184-1.297-.513a1.71 1.71 0 0 1-.537-1.237V3.5c0-.464.194-.91.537-1.237.344-.329.81-.513 1.297-.513h8.25c.486 0 .952.184 1.296.513.344.328.537.773.537 1.237v.875"
+      stroke="#8A99B1"
+      strokeWidth={3}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default Copy
