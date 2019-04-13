3e5a30b0c8f59f3e63c385b67bd6df9fdda684ea
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-04-13 08:38:46 +0430

    Add theme props

---
+++ utils/theme.js
@@ -0,0 +1,22 @@
+export const styledTheme = {
+  //sizes
+  containerNarrowWidth: 1200,
+  sideSpace: 10,
+  // Colors
+  black: 'black',
+  black1: '#212121',
+  black2: '#2c353c',
+
+  gray: '#475158',
+  // Fonts
+  fontSystem: `-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
+  fontText: `'IBM Plex Sans'`,
+  fontTitle: `Alegreya Sans`,
+
+  fontMedium14: 14,
+  fontMedium15: 15,
+  fontMedium18: 18,
+  fontSuperLarge42: 42
+
+  // Z-Indexes
+}
