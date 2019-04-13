ba792a7a5f7f81bc62b841cb17a03497d7755109
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-04-13 08:39:29 +0430

    Add Shapes Svg components

---
+++ components/vectors/Shapes.js
@@ -0,0 +1,111 @@
+import React from 'react'
+
+export const Square = props => (
+  <svg width={179} height={179} fill="none" {...props}>
+    <rect
+      y={71.606}
+      width={129}
+      height={129}
+      rx={16}
+      transform="rotate(-33.716 0 71.606)"
+      fill="#6EFDC9"
+    />
+  </svg>
+)
+
+export const Triangle = props => (
+  <svg width={108} height={110} fill="none" {...props}>
+    <path
+      d="M22.469 43.398c-2.878-5.445 1.217-11.963 7.372-11.733l66.637 2.49c6.154.23 9.751 7.036 6.475 12.25L67.478 102.87c-3.277 5.215-10.97 4.927-13.847-.517L22.469 43.398z"
+      fill="#FFF06A"
+    />
+  </svg>
+)
+
+export const HalfCircle = props => (
+  <svg width={162} height={141} fill="none" {...props}>
+    <path
+      d="M6.723 74.764L115.894 4.337c3.712-2.395 8.597-1.313 10.498 2.675 8.2 17.207 23.208 62.403-25.534 93.847-48.741 31.443-83.73-.863-96.026-15.427-2.85-3.376-1.822-8.273 1.89-10.668z"
+      fill="#FFD28D"
+    />
+  </svg>
+)
+
+export const MultiCircles = props => (
+  <svg width={109} height={75} fill="none" {...props}>
+    <circle cx={57.5} cy={16.5} r={13.5} fill="#DEF3FF" />
+    <circle cx={19} cy={19} r={19} fill="#77ADFF" />
+    <circle cx={34.5} cy={10.5} r={1.5} fill="#89D5FF" />
+    <circle cx={43.532} cy={10.5} r={1.5} fill="#89D5FF" />
+    <circle cx={52.565} cy={10.5} r={1.5} fill="#89D5FF" />
+    <circle cx={61.597} cy={10.5} r={1.5} fill="#89D5FF" />
+    <circle cx={70.629} cy={10.5} r={1.5} fill="#89D5FF" />
+    <circle cx={34.5} cy={19.531} r={1.5} fill="#89D5FF" />
+    <circle cx={43.532} cy={19.531} r={1.5} fill="#89D5FF" />
+    <circle cx={52.565} cy={19.531} r={1.5} fill="#89D5FF" />
+    <circle cx={61.597} cy={19.531} r={1.5} fill="#89D5FF" />
+    <circle cx={70.629} cy={19.531} r={1.5} fill="#89D5FF" />
+    <circle cx={34.5} cy={28.564} r={1.5} fill="#89D5FF" />
+    <circle cx={43.532} cy={28.564} r={1.5} fill="#89D5FF" />
+    <circle cx={52.565} cy={28.564} r={1.5} fill="#89D5FF" />
+    <circle cx={61.597} cy={28.564} r={1.5} fill="#89D5FF" />
+    <circle cx={70.629} cy={28.564} r={1.5} fill="#89D5FF" />
+    <circle cx={34.5} cy={37.598} r={1.5} fill="#89D5FF" />
+    <circle cx={43.532} cy={37.598} r={1.5} fill="#89D5FF" />
+    <circle cx={52.565} cy={37.598} r={1.5} fill="#89D5FF" />
+    <circle cx={61.597} cy={37.598} r={1.5} fill="#89D5FF" />
+    <circle cx={70.629} cy={37.598} r={1.5} fill="#89D5FF" />
+    <circle cx={34.5} cy={46.629} r={1.5} fill="#89D5FF" />
+    <circle cx={43.532} cy={46.629} r={1.5} fill="#89D5FF" />
+    <circle cx={52.565} cy={46.629} r={1.5} fill="#89D5FF" />
+    <circle cx={61.597} cy={46.629} r={1.5} fill="#89D5FF" />
+    <circle cx={70.629} cy={46.629} r={1.5} fill="#89D5FF" />
+    <circle cx={79.5} cy={10.5} r={1.5} fill="#89D5FF" />
+    <circle cx={88.532} cy={10.5} r={1.5} fill="#89D5FF" />
+    <circle cx={79.5} cy={19.531} r={1.5} fill="#89D5FF" />
+    <circle cx={88.532} cy={19.531} r={1.5} fill="#89D5FF" />
+    <circle cx={79.5} cy={28.564} r={1.5} fill="#89D5FF" />
+    <circle cx={88.532} cy={28.564} r={1.5} fill="#89D5FF" />
+    <circle cx={79.5} cy={37.598} r={1.5} fill="#89D5FF" />
+    <circle cx={88.532} cy={37.598} r={1.5} fill="#89D5FF" />
+    <circle cx={79.5} cy={46.629} r={1.5} fill="#89D5FF" />
+    <circle cx={88.532} cy={46.629} r={1.5} fill="#89D5FF" />
+    <circle cx={79.5} cy={55.5} r={1.5} fill="#89D5FF" />
+    <circle cx={88.532} cy={55.5} r={1.5} fill="#89D5FF" />
+    <circle cx={34.5} cy={55.5} r={1.5} fill="#89D5FF" />
+    <circle cx={43.532} cy={55.5} r={1.5} fill="#89D5FF" />
+    <circle cx={52.565} cy={55.5} r={1.5} fill="#89D5FF" />
+    <circle cx={61.597} cy={55.5} r={1.5} fill="#89D5FF" />
+    <circle cx={70.629} cy={55.5} r={1.5} fill="#89D5FF" />
+    <circle cx={34.5} cy={64.5} r={1.5} fill="#89D5FF" />
+    <circle cx={43.532} cy={64.5} r={1.5} fill="#89D5FF" />
+    <circle cx={52.565} cy={64.5} r={1.5} fill="#89D5FF" />
+    <circle cx={61.597} cy={64.5} r={1.5} fill="#89D5FF" />
+    <circle cx={70.629} cy={64.5} r={1.5} fill="#89D5FF" />
+    <circle cx={79.5} cy={64.5} r={1.5} fill="#89D5FF" />
+    <circle cx={88.532} cy={64.5} r={1.5} fill="#89D5FF" />
+    <circle cx={79.5} cy={73.371} r={1.5} fill="#89D5FF" />
+    <circle cx={88.532} cy={73.371} r={1.5} fill="#89D5FF" />
+    <circle cx={97.5} cy={10.5} r={1.5} fill="#89D5FF" />
+    <circle cx={106.532} cy={10.5} r={1.5} fill="#89D5FF" />
+    <circle cx={97.5} cy={19.531} r={1.5} fill="#89D5FF" />
+    <circle cx={106.532} cy={19.531} r={1.5} fill="#89D5FF" />
+    <circle cx={97.5} cy={28.564} r={1.5} fill="#89D5FF" />
+    <circle cx={106.532} cy={28.564} r={1.5} fill="#89D5FF" />
+    <circle cx={97.5} cy={37.598} r={1.5} fill="#89D5FF" />
+    <circle cx={106.532} cy={37.598} r={1.5} fill="#89D5FF" />
+    <circle cx={97.5} cy={46.629} r={1.5} fill="#89D5FF" />
+    <circle cx={106.532} cy={46.629} r={1.5} fill="#89D5FF" />
+    <circle cx={97.5} cy={55.5} r={1.5} fill="#89D5FF" />
+    <circle cx={106.532} cy={55.5} r={1.5} fill="#89D5FF" />
+    <circle cx={97.5} cy={64.5} r={1.5} fill="#89D5FF" />
+    <circle cx={106.532} cy={64.5} r={1.5} fill="#89D5FF" />
+    <circle cx={97.5} cy={73.371} r={1.5} fill="#89D5FF" />
+    <circle cx={106.532} cy={73.371} r={1.5} fill="#89D5FF" />
+    <circle cx={34.5} cy={73.371} r={1.5} fill="#89D5FF" />
+    <circle cx={43.532} cy={73.371} r={1.5} fill="#89D5FF" />
+    <circle cx={52.565} cy={73.371} r={1.5} fill="#89D5FF" />
+    <circle cx={61.597} cy={73.371} r={1.5} fill="#89D5FF" />
+    <circle cx={70.629} cy={73.371} r={1.5} fill="#89D5FF" />
+  </svg>
+)
