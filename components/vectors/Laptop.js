868ed4ce489274890ce97dfd911f41d18eca98c8
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-04-13 08:36:44 +0430

    Add some logo customers

---
+++ components/vectors/Laptop.js
@@ -0,0 +1,247 @@
+import React from 'react'
+
+export const Laptop = props => (
+  <svg width={713} height={410} fill="none" {...props}>
+    <g filter="url(#prefix__filter0_di)">
+      <path
+        d="M67 389.5c0 2 1 4.5 1.5 5h576c1-1 1.5-3.5 1.5-5V22c0-10.493-8.507-19-19-19H86c-10.493 0-19 8.507-19 19v367.5z"
+        fill="#000"
+      />
+      <path
+        d="M67.793 395.207l.293.293h576.828l.293-.293c.693-.693 1.123-1.782 1.384-2.762.271-1.015.409-2.102.409-2.945V22c0-11.046-8.954-20-20-20H86c-11.046 0-20 8.954-20 20v367.5c0 1.145.282 2.382.614 3.379.168.504.356.967.542 1.349.17.348.386.729.637.979z"
+        stroke="#7E8082"
+        strokeWidth={2}
+      />
+    </g>
+    <g filter="url(#prefix__filter1_i)">
+      <path
+        d="M67 381.5h578v7a6 6 0 0 1-6 6H73a6 6 0 0 1-6-6v-7z"
+        fill="#1B1B1B"
+      />
+    </g>
+    <path
+      d="M67.5 382h577v6.5a5.5 5.5 0 0 1-5.5 5.5H73a5.5 5.5 0 0 1-5.5-5.5V382z"
+      stroke="#252526"
+    />
+    <path d="M0 398.5v-4h713v4H0z" fill="#B9BABF" />
+    <path d="M0 398.5v-4h713v4H0z" fill="url(#prefix__paint0_linear)" />
+    <path d="M0 398.5v-4h713v4H0z" fill="url(#prefix__paint1_linear)" />
+    <path
+      d="M75 409.5c-15 0-56-1.5-75-11h295.5l4.614 1.538a9.001 9.001 0 0 0 2.847.462h106.686c.897 0 1.789-.134 2.646-.398l5.207-1.602H713c-13 6-43.5 11-68 11H75z"
+      fill="url(#prefix__paint2_linear)"
+    />
+    <path
+      d="M296 398.5v-4h121.5v4l-4.775 1.469a11.998 11.998 0 0 1-3.529.531H303.947c-1.289 0-2.571-.208-3.794-.616L296 398.5z"
+      fill="#B1B2B7"
+    />
+    <path
+      d="M296 398.5v-4h121.5v4l-4.775 1.469a11.998 11.998 0 0 1-3.529.531H303.947c-1.289 0-2.571-.208-3.794-.616L296 398.5z"
+      fill="url(#prefix__paint3_linear)"
+    />
+    <path
+      d="M296 398.5v-4h121.5v4l-4.775 1.469a11.998 11.998 0 0 1-3.529.531H303.947c-1.289 0-2.571-.208-3.794-.616L296 398.5z"
+      fill="url(#prefix__paint4_linear)"
+    />
+    <path
+      d="M339.081 390.926V385h-.893l-2.13 4.986h-.034l-2.13-4.986H333v5.926h.718v-4.567h.03l1.968 4.567h.65l1.967-4.567h.03v4.567h.718zM341.414 390.392c-.534 0-.889-.267-.889-.673 0-.403.342-.657.941-.694l1.189-.07v.349c0 .616-.539 1.088-1.241 1.088zm-.166.608c.598 0 1.163-.32 1.428-.809h.017v.735h.706v-2.953c0-.85-.659-1.392-1.702-1.392-1.052 0-1.711.567-1.762 1.306h.718c.094-.419.454-.678 1.027-.678.615 0 .975.313.975.834v.357l-1.275.07c-1.026.058-1.612.518-1.612 1.249 0 .768.594 1.281 1.48 1.281zM347.951 388.055c-.094-.768-.757-1.474-1.868-1.474-1.228 0-2.045.863-2.045 2.21 0 1.375.821 2.209 2.053 2.209 1.009 0 1.754-.6 1.869-1.45h-.731c-.129.514-.526.822-1.134.822-.769 0-1.3-.588-1.3-1.581 0-.97.522-1.582 1.292-1.582.654 0 1.03.382 1.137.846h.727zM351.003 390.926c1.313 0 2.091-.62 2.091-1.663 0-.789-.539-1.355-1.377-1.458v-.033c.595-.103 1.065-.678 1.065-1.294 0-.887-.714-1.478-1.771-1.478h-2.339v5.926h2.331zm-1.561-5.273h1.394c.749 0 1.168.324 1.168.912 0 .64-.496.989-1.407.989h-1.155v-1.901zm0 4.62v-2.082h1.373c.983 0 1.488.349 1.488 1.035 0 .686-.488 1.047-1.416 1.047h-1.445zM355.607 391c1.227 0 2.057-.85 2.057-2.209 0-1.364-.83-2.21-2.057-2.21-1.232 0-2.062.846-2.062 2.21 0 1.359.83 2.209 2.062 2.209zm0-.628c-.787 0-1.305-.575-1.305-1.581 0-1.007.518-1.582 1.305-1.582.787 0 1.3.575 1.3 1.582 0 1.006-.513 1.581-1.3 1.581zM360.172 391c1.228 0 2.057-.85 2.057-2.209 0-1.364-.829-2.21-2.057-2.21-1.231 0-2.061.846-2.061 2.21 0 1.359.83 2.209 2.061 2.209zm0-.628c-.787 0-1.304-.575-1.304-1.581 0-1.007.517-1.582 1.304-1.582s1.3.575 1.3 1.582c0 1.006-.513 1.581-1.3 1.581zM363.677 388.552V385h-.744v5.926h.744v-1.61l.415-.378 1.651 1.988h.864l-1.989-2.427 1.929-1.844h-.903l-1.967 1.897zM372.893 390.926h.809L371.427 385h-.762l-2.275 5.926h.808l.621-1.696h2.454l.62 1.696zm-1.86-5.027h.026l.988 2.703h-2.002l.988-2.703zM374.243 390.926h.744v-4.271h-.744v4.271zm.372-5.031a.455.455 0 0 0 .466-.447.453.453 0 0 0-.466-.448.455.455 0 0 0-.466.448c0 .246.209.447.466.447zM375.904 390.926h.745v-2.661c0-.604.376-1.002.953-1.002.18 0 .338.02.398.041v-.694c-.06-.008-.171-.029-.299-.029-.522 0-.933.3-1.078.772h-.017v-.698h-.702v4.271z"
+      fill="#C7C7C7"
+    />
+    <path fill="url(#prefix__paint5_linear)" d="M81 26h551v344H81z" />
+    <circle cx={357} cy={15} r={1} fill="#fff" fillOpacity={0.14} />
+    <path fill="#fff" fillOpacity={0.11} d="M81 26h551v11H81z" />
+    <path
+      d="M168 353a4 4 0 0 1 4-4h378a4 4 0 0 1 4 4v17H168v-17z"
+      fill="#fff"
+      fillOpacity={0.03}
+    />
+    <path
+      d="M554 370v-17a4 4 0 0 0-4-4H172a4 4 0 0 0-4 4v17"
+      stroke="#fff"
+      strokeOpacity={0.08}
+    />
+    <rect
+      x={600}
+      y={30}
+      width={18}
+      height={4}
+      rx={2}
+      fill="#fff"
+      fillOpacity={0.28}
+    />
+    <rect
+      x={591}
+      y={30}
+      width={4}
+      height={4}
+      rx={2}
+      fill="#fff"
+      fillOpacity={0.28}
+    />
+    <rect
+      x={584}
+      y={30}
+      width={4}
+      height={4}
+      rx={2}
+      fill="#fff"
+      fillOpacity={0.28}
+    />
+    <rect
+      x={577}
+      y={30}
+      width={4}
+      height={4}
+      rx={2}
+      fill="#fff"
+      fillOpacity={0.28}
+    />
+    <rect
+      x={622}
+      y={30}
+      width={6}
+      height={4}
+      rx={2}
+      fill="#fff"
+      fillOpacity={0.28}
+    />
+    <circle cx={87} cy={32} r={3} fill="#fff" fillOpacity={0.26} />
+    <defs>
+      <filter
+        id="prefix__filter0_di"
+        x={64}
+        y={0}
+        width={585}
+        height={397.5}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        />
+        <feOffset />
+        <feGaussianBlur stdDeviation={0.5} />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.52 0" />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow" />
+        <feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+        <feColorMatrix
+          in="SourceAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset />
+        <feGaussianBlur stdDeviation={1.5} />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2={-1} k3={1} />
+        <feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.9 0" />
+        <feBlend in2="shape" result="effect2_innerShadow" />
+      </filter>
+      <filter
+        id="prefix__filter1_i"
+        x={67}
+        y={381.5}
+        width={578}
+        height={13}
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+        <feColorMatrix
+          in="SourceAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset />
+        <feGaussianBlur stdDeviation={0.5} />
+        <feComposite in2="hardAlpha" operator="arithmetic" k2={-1} k3={1} />
+        <feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.38 0" />
+        <feBlend in2="shape" result="effect1_innerShadow" />
+      </filter>
+      <linearGradient
+        id="prefix__paint0_linear"
+        x1={40}
+        y1={396}
+        x2={0}
+        y2={396.5}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#fff" stopOpacity={0} />
+        <stop offset={0.273} stopColor="#fff" stopOpacity={0.26} />
+        <stop offset={0.372} stopColor="#fff" stopOpacity={0.29} />
+        <stop offset={0.715} stopColor="#66676A" stopOpacity={0.876} />
+        <stop offset={0.788} stopColor="#46474B" />
+        <stop offset={0.829} stopColor="#494A4F" />
+        <stop offset={1} stopColor="#AFAFAF" stopOpacity={0} />
+      </linearGradient>
+      <linearGradient
+        id="prefix__paint1_linear"
+        x1={675}
+        y1={394.5}
+        x2={713}
+        y2={394.5}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#fff" stopOpacity={0} />
+        <stop offset={0.273} stopColor="#fff" stopOpacity={0.26} />
+        <stop offset={0.372} stopColor="#fff" stopOpacity={0.29} />
+        <stop offset={0.715} stopColor="#66676A" stopOpacity={0.876} />
+        <stop offset={0.788} stopColor="#46474B" />
+        <stop offset={0.829} stopColor="#494A4F" />
+        <stop offset={1} stopColor="#AFAFAF" stopOpacity={0} />
+      </linearGradient>
+      <linearGradient
+        id="prefix__paint2_linear"
+        x1={356.5}
+        y1={398.5}
+        x2={356.5}
+        y2={409.5}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#928D8D" />
+        <stop offset={1} stopColor="#F0F1F5" />
+      </linearGradient>
+      <linearGradient
+        id="prefix__paint3_linear"
+        x1={322}
+        y1={398.5}
+        x2={294.142}
+        y2={396.508}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#fff" stopOpacity={0} />
+        <stop offset={0.259} stopColor="#fff" stopOpacity={0.09} />
+        <stop offset={0.595} stopColor="#fff" stopOpacity={0.29} />
+        <stop offset={0.784} stopColor="#5B5C60" />
+        <stop offset={0.857} stopColor="#494A4E" />
+        <stop offset={1} stopColor="#AFAFAF" stopOpacity={0} />
+      </linearGradient>
+      <linearGradient
+        id="prefix__paint4_linear"
+        x1={394}
+        y1={398.501}
+        x2={418.873}
+        y2={400.28}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#fff" stopOpacity={0} />
+        <stop offset={0.259} stopColor="#fff" stopOpacity={0.09} />
+        <stop offset={0.595} stopColor="#fff" stopOpacity={0.16} />
+        <stop offset={0.784} stopColor="#5B5C60" />
+        <stop offset={0.857} stopColor="#494A4E" />
+        <stop offset={1} stopColor="#AFAFAF" stopOpacity={0} />
+      </linearGradient>
+      <linearGradient
+        id="prefix__paint5_linear"
+        x1={356.5}
+        y1={26}
+        x2={356.5}
+        y2={370}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset={0.182} stopColor="#fff" stopOpacity={0.13} />
+        <stop offset={1} stopColor="#fff" stopOpacity={0.18} />
+      </linearGradient>
+    </defs>
+  </svg>
+)
