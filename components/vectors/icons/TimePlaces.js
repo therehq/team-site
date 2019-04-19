0a4608d874bc6d9636cc7f8ecce607ffac4277ab
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-04-19 14:26:48 +0430

    Add section TimePlaces

---
+++ components/vectors/icons/TimePlaces.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export const TimePlaces = props => (
+  <svg width={28} height={28} fill="none" {...props}>
+    <path
+      d="M14 .03c-7.72 0-14 6.28-14 14 0 7.719 6.28 13.943 14 13.943s14-6.224 14-13.944c0-7.72-6.28-14-14-14zm.933 14.409a.934.934 0 0 1-.933.933H6.533a.934.934 0 0 1 0-1.866h6.534V3.239a.934.934 0 0 1 1.866 0v11.2z"
+      fill="#000"
+    />
+    <path
+      d="M14 .03c-7.72 0-14 6.28-14 14 0 7.719 6.28 13.943 14 13.943s14-6.224 14-13.944c0-7.72-6.28-14-14-14zm.933 14.409a.934.934 0 0 1-.933.933H6.533a.934.934 0 0 1 0-1.866h6.534V3.239a.934.934 0 0 1 1.866 0v11.2z"
+      fill="#C9D7E7"
+    />
+    <path
+      d="M14 .03c-7.72 0-14 6.28-14 14 0 7.719 6.28 13.943 14 13.943s14-6.224 14-13.944c0-7.72-6.28-14-14-14zm.933 14.409a.934.934 0 0 1-.933.933H6.533a.934.934 0 0 1 0-1.866h6.534V3.239a.934.934 0 0 1 1.866 0v11.2z"
+      fill="url(#TimePlaces__paint0_linear)"
+    />
+    <defs>
+      <linearGradient
+        id="TimePlaces__paint0_linear"
+        x1={14}
+        y1={0.029}
+        x2={14.556}
+        y2={14.56}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopOpacity={0.13} />
+        <stop offset={1} stopColor="#fff" stopOpacity={0} />
+        <stop offset={1} stopOpacity={0} />
+      </linearGradient>
+    </defs>
+  </svg>
+)
