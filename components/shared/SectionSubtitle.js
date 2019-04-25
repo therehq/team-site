aed8472ce29702f5e88d6f596415f2c161fce9d6
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-04-25 15:13:38 +0430

    Fix some porps

--- components/shared/SectionSubtitle.js
+++ components/shared/SectionSubtitle.js
@@ -1,16 +1,18 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-export const SectionSubtitle = styled.div`
-  max-width: 660px;
+export const SectionSubtitle = styled.span`
+  display: block;
+  max-width: ${p => (p.maxWidth ? p.maxWidth : 660)}px;
+  margin-top: ${p => (p.top ? p.top : '0')}px;
 
   font-family: ${p => p.theme.fontText};
   font-size: ${p => p.theme.fontMedium18}px;
   font-style: normal;
   font-weight: 400;
   line-height: 1.44;
-  text-align: center;
   letter-spacing: 0.01em;
+  text-align: ${p => (p.align ? p.align : 'center')};
 
   color: ${p => p.theme.gray};
 `
