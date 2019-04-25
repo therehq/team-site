ba62d73dceb40017ea739a1a8ea69af77a36c288
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-04-25 15:13:26 +0430

    Add mobile function

---
+++ components/style/mobile.js
@@ -0,0 +1,19 @@
+import { css } from 'styled-components'
+
+export const mobile = body => css`
+  @media screen and (max-width: 1000px) {
+    ${body};
+  }
+`
+
+export const desktop = body => css`
+  @media screen and (min-width: 1001px) {
+    ${body};
+  }
+`
+
+export const retina = style => css`
+  @media (-webkit-min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
+    ${style};
+  }
+`
