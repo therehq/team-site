bbd202bba644f94e27807222a1729963a0053f52
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-04-25 15:10:50 +0430

    Add all components ti index

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import { Header } from '../components/Header'
 import { OurCustomers } from '../components/OurCustomers/OurCustomers'
 import { Screenshots } from '../components/screenshots/Screenshots'
 import { Scheduling } from '../components/scheduling/Scheduling'
+import { Features } from '../components/Features'
+import { PeopleAvatars } from '../components/PeopleAvatars'
+import { Quotes } from '../components/Quotes'
 import Space from '../components/shared/Space'
 
 function Home() {
@@ -13,6 +16,9 @@ function Home() {
       <OurCustomers />
       <Screenshots />
       <Scheduling />
+      <Features />
+      <PeopleAvatars />
+      <Quotes />
       <Space height={100} />
     </>
   )
