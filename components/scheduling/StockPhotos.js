492e90919c609fde22db2a117da99bb894efaef7
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-05-02 17:45:42 +0430

    Add StockPhotos to project

---
+++ components/scheduling/StockPhotos.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const StockPhotos = () => (
+  <Wrapper>
+    <Row>
+      <PhotoBox
+        src="../../static/scheduling/video_call.svg"
+        width={127}
+        height={110}
+        borderRadius="0"
+      />
+      <PhotoBox src="../../static/scheduling/3.jpg" width={95} />
+      <PhotoBox src="../../static/scheduling/2.jpg" width={78} />
+      <PhotoBox src="../../static/scheduling/1.jpg" width={128} />
+    </Row>
+    <Row>
+      <PhotoBox
+        src="../../static/scheduling/7.jpg"
+        width={108}
+        marginRight={10}
+      />
+      <PhotoBox src="../../static/scheduling/6.jpg" width={130} />
+      <PhotoBox src="../../static/scheduling/5.jpg" width={117} />
+      <PhotoBox src="../../static/scheduling/4.jpg" width={89} />
+    </Row>
+  </Wrapper>
+)
+
+const Wrapper = styled.div`
+  min-width: 580px;
+`
+
+const Row = styled.div`
+  display: flex;
+  margin-bottom: 26px;
+`
+
+const PhotoBox = styled.div`
+  width: ${p => p.width}px;
+  height: ${p => (p.height ? p.height : 98)}px;
+
+  background: url(${p => p.src});
+  border-radius: ${p => (p.borderRadius ? p.borderRadius : 25)}px;
+  background-size: cover;
+  margin-left: 28px;
+  margin-right: ${p => (p.marginRight ? p.marginRight : 0)}px;
+`
