0e05c5b700e96996d621697f241a4cf85c1cad5a
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-05-08 08:27:59 +0430

    Add: complete ArrowRight component

---
+++ components/vectors/ArrowRight.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const ArrowRight = props => (
+  <svg width={7} height={12} fill="none" {...props}>
+    <path
+      d="M1 1l5 5-5 5"
+      stroke="#6B6B6B"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
