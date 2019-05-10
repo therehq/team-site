776466edd78fd6ea97ca9342478e1add8f83de55
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-05-10 11:56:09 +0430

    Chore: add some packages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+const withCSS = require('@zeit/next-css')
+
+module.exports = withCSS({
+  webpack: config => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
     // Important: return the modified config
@@ -10,10 +12,5 @@ module.exports = {
     // Example using webpack option
     // config.plugins.push(new webpack.IgnorePlugin(//__tests__//));
     return config
-  },
-  webpackDevMiddleware: config => {
-    // Perform customizations to webpack dev middleware config
-    // Important: return the modified config
-    return config
   }
-}
+})
