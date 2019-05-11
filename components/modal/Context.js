a8d85323577b8066d1be580b21a3fa4249f5671a
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-05-11 12:51:46 +0430

    Add email contex

--- components/modal/Context.js
+++ components/modal/Context.js
@@ -5,11 +5,19 @@ export const ModalContext = createContext(false)
 
 export const ModalProvider = ({ children }) => {
   const [isOpen, setOpenState] = useState(false)
+  const [email, setEmailState] = useState('')
 
   return (
-    <ModalContext.Provider value={{ isOpen, setOpenState }}>
+    <ModalContext.Provider
+      value={{ isOpen, setOpenState, email, setEmailState }}
+    >
       {children}
-      <Modal isOpen={isOpen} setOpenState={setOpenState} />
+      <Modal
+        isOpen={isOpen}
+        setOpenState={setOpenState}
+        defaultEmail={email}
+        setEmailState={setEmailState}
+      />
     </ModalContext.Provider>
   )
 }
