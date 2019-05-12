50f02f9e7c906752de4e4109a1e1c5760b31f559
Behnam Rajabifard <b.rajabifard@gmail.com>
2019-05-12 14:00:45 +0430

    Add some styles

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import NextLink from 'next/link'
 import styled from 'styled-components'
 
 // Locals
 import { Wrapper } from './shared/Containers'
 import { There } from './vectors/logos/There'
+import { ModalContext } from './modal/Context'
 
-export const Nav = () => (
-  <StyledWrapper>
-    <There />
-    <NextLink href="#">
-      <RequestButton href="#">Request Access</RequestButton>
-    </NextLink>
-  </StyledWrapper>
-)
+export const Nav = () => {
+  const { setOpenState } = useContext(ModalContext)
+  return (
+    <StyledWrapper>
+      <There />
+      <RequestButton onClick={() => setOpenState(true)}>
+        Request Access
+      </RequestButton>
+    </StyledWrapper>
+  )
+}
 
 const StyledWrapper = styled(Wrapper)`
   display: flex;
@@ -21,18 +25,24 @@ const StyledWrapper = styled(Wrapper)`
   align-items: center;
 `
 
-const RequestButton = styled.a`
+const RequestButton = styled.button`
   font-family: ${p => p.theme.fontText};
   font-size: ${p => p.theme.fontMedium18}px;
   font-style: normal;
   font-weight: 600;
   line-height: 0.86;
-  transition: 50ms all;
-
+  border-radius: 3px;
+  user-select: none;
+  transition: background 120ms ease-in 0s;
+  padding: 8px 7px;
   color: ${p => p.theme.black1};
+  cursor: pointer;
+  outline: none;
+  border: none;
 
   &:hover {
-    padding-right: 5px;
+    background: rgba(55, 53, 47, 0.08);
+    /* padding-right: 5px; */
     color: #333;
   }
 `
