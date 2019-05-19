7aab31900471e42e5f04747a5028f31f25bddc17
Mo Rajabifard <mo.rajbi@gmail.com>
2019-05-19 14:13:45 +0430

    Minor ui update to There news

--- components/ThereNews.js
+++ components/ThereNews.js
@@ -6,7 +6,7 @@ import { Container } from './shared/Containers'
 
 export const ThereNews = () => (
   <Wrapper>
-    Looking for our free time zone menubar app?{' '}
+    Looking for our free <strong>time zone menubar app?</strong>{' '}
     <Button href="https://there.pm">Get it on there.pm</Button>
   </Wrapper>
 )
@@ -16,12 +16,13 @@ const Wrapper = styled.div`
 
   font-family: ${p => p.theme.fontText};
   font-size: 16px;
-  line-height: 1.5;
+  line-height: 1.3;
   text-align: center;
 
   color: rgb(255, 255, 255);
   background: radial-gradient(772.5px at 50% -650%, #214967 0%, #0d2d45 100%);
-  padding: 13px;
+  padding-top: 10px;
+  padding-bottom: 13px;
   position: relative;
 `
 
