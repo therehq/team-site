86647521d9dce57befa9fe91cb97ae706f384d78
Mo Rajabifard <mo.rajbi@gmail.com>
2019-05-21 07:55:37 +0430

    Update Twitter message

--- utils/share.js
+++ utils/share.js
@@ -1,6 +1,6 @@
 export const openTwitterModal = () => {
   return window.open(
-    'https://twitter.com/intent/tweet?url=https%3A%2F%2Fthere.team&text=I%27m%20on%20the%20waitlist%20of%20@ThereHQ.%20It%27s%20for%20remote%20teams%21%20',
+    'https://twitter.com/intent/tweet?url=https://there.team&text=I%27m%20on%20the%20waitlist%20of%20%40ThereHQ!%20%E2%98%BA%EF%B8%8F%20Makes%20remote%20work%20smoother',
     'Twitter',
     'width=600,height=300'
   )
